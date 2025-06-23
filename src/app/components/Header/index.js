801f19ef8d8071dd143348a7c5a9e9f20fed4a6a
Olimxon <olimxon401gmail.com>
2025-06-23 21:47:11 +0500

    ne sdamsz

---
+++ src/app/components/Header/index.js
@@ -0,0 +1,9 @@
+import './header.scss';
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Не сдамся!!!!!!!!!!!!!!!</h1>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import React from 'react';
+import Header from './components/Header';
 export default function Home() {
   return (
     <div>
-        
+      <Header />
     </div>
   );
 }
